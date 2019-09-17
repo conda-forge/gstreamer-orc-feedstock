ddaf59ed502680cbcba7e729a8408a0c12de0d34
Sebastian Luna-Valero <sebastian.luna.valero@gmail.com>
2019-09-17 18:45:27 +0200

    try --includedir

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,6 @@
 set -euo pipefail
 
 meson buildir
-meson configure buildir --prefix=${PREFIX} --libdir=${PREFIX}/lib
+meson configure buildir --prefix=${PREFIX} --libdir=${PREFIX}/lib --includedir=${PREFIX}/include
 ninja -C buildir
 ninja -C buildir install
