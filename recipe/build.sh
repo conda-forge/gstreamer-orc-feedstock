e1845e55d66da6884f31601d19478ecd5490f0fd
Marcel Bargull <marcel.bargull@udo.edu>
2019-12-02 10:45:59 +0100

    Patch meson.build to use install_rpath

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-meson setup buildir --prefix=${PREFIX} --libdir=${PREFIX}/lib
+meson setup buildir --prefix=${PREFIX} --libdir=lib
 ninja -C buildir
 ninja -C buildir install
