65afc6983fc32f6953b64b6a6a58d314f991a00b
Ryan Volz <ryan.volz@gmail.com>
2021-05-06 12:28:59 -0400

    Move meson to build deps, try to fix osx-arm64 cross-compilation.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-meson setup buildir --prefix=${PREFIX} --libdir=lib
+meson setup buildir ${MESON_ARGS} --prefix=${PREFIX} --libdir=lib
 ninja -C buildir
 ninja -C buildir install
