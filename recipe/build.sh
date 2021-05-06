4af29cf79d28deedb25e552c77c0cf6b624fd612
Ryan Volz <ryan.volz@gmail.com>
2021-05-06 15:13:20 -0400

    Don't install static libraries (CFEP 18).

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,3 +4,7 @@ set -eo pipefail
 meson setup buildir ${MESON_ARGS} --buildtype=release --prefix=${PREFIX} -Dlibdir=lib
 ninja -C buildir
 ninja -C buildir install
+
+# don't install static library (CFEP #18)
+# (easier to delete than patch source to not build it)
+rm -f $PREFIX/liborc-test*.a
